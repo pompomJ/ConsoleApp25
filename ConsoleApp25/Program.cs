1570d09fd778405b7d601c42cf1cee4965482411
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-15 13:19:47 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; true; i++)
+            {
+                if (i < 8)
+                {
+                    continue; //カウンターを更新してループの先頭に戻る
+                }
+                else if (i == 10)
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
